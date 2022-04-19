9b3f4c4b0f2599d3d635b5b0984d79ca63400676
ColinMcDonough <70282607+ColinMcDonough@users.noreply.github.com>
2022-04-18 20:04:28 -0400

    Add files via upload

--- checkQuad.m
+++ checkQuad.m
@@ -1,92 +1,133 @@
-function checkQuad(currentPosition,desiredQuad)
+function [correctQuad,counter] = checkQuad(currentPosition,desiredQuad,counter)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 %disp('function run \n')
 %disp(desiredQuad)
-
+correctQuad = 0;
+cordLimits = [320 240];
 % Frame size: [640 480]
 % mid X: 320
 % mid Y: 240
-cordLimits = [320 240];
 
-% currentPosition is (x1,y1), (x2,y2), (x3,y3), (x4,y4), but in an array so
-% currentPosition = [1,2, 3,4, 5,6, 7,8]
+
 
 switch desiredQuad
 
     % Check Top Left
     % Bottom right point of rectangle (5,6)
+
     case '1'
         if(currentPosition(5) > cordLimits(:,1) && currentPosition(6) > cordLimits(:,2))
             %move both up and right
             disp('move right and up')
+            counter = 0
+            
         elseif(currentPosition(5) > cordLimits(:,1))
             %move right
             disp('move right')
-
+            counter = 0
+            
+        
         elseif(currentPosition(6) > cordLimits(:,2))
             %move up
             disp('move up')
+            counter = 0
+            
+
         else
             %correct quad
             disp('Correct Quad')
+            counter = counter + 1
+            if(counter >= 60)
+                correctQuad = 1;
+            end
         end
-
         % Check Top Right
         % Bottom Left point of rectangle (7,8)
     case '2'
         if(currentPosition(7) < cordLimits(:,1) && currentPosition(8) > cordLimits(:,2))
-            %move both up and left
+            %move both up and right
             disp('move left and up')
+            counter = 0;
+            
+
         elseif(currentPosition(7) < cordLimits(:,1))
-            %move left
+            %move right
             disp('move left')
+            counter = 0;
+            
 
         elseif(currentPosition(8) > cordLimits(:,2))
             %move up
             disp('move up')
+            counter = 0;
+            
         else
             %correct quad
             disp('Correct Quad')
+            counter = counter + 1;
+            if(counter >= 60)
+                correctQuad = 1;
+            end
         end
 
         % Check Bottom Left
         % Top Right point of rectangle (3,4)
     case '3'
-        if(currentPosition(3) > cordLimits(:,1) && currentPosition(4) < cordLimits(:,2))
-            %move both right and down
+        if(currentPosition(1) < cordLimits(:,1) && currentPosition(2) < cordLimits(:,2))
+            %move both up and right
             disp('move right and down')
-        elseif(currentPosition(3) > cordLimits(:,1))
+            counter = 0;
+            
+        elseif(currentPosition(1) < cordLimits(:,1))
             %move right
             disp('move right')
-
-        elseif(currentPosition(4) < cordLimits(:,2))
-            %move down
+            counter = 0;
+            
+        
+        elseif(currentPosition(2) < cordLimits(:,2))
+            %move up
             disp('move down')
+            counter = 0;
+            
         else
             %correct quad
             disp('Correct Quad')
+            counter = counter + 1;
+            if(counter >= 60)
+                correctQuad = 1;
+            end
         end
 
         % Check Bottom Right
         % Top Left point of rectangle (1,2)
     case '4'
-        if(currentPosition(1) < cordLimits(:,1) && currentPosition(2) < cordLimits(:,2))
-            %move both down and left
+        if(currentPosition(3) < cordLimits(:,1) && currentPosition(4) < cordLimits(:,2))
+            %move both up and right
             disp('move left and down')
-        elseif(currentPosition(1) < cordLimits(:,1))
+            counter = 0;
+            
+        elseif(currentPosition(3) < cordLimits(:,1))
             %move right
             disp('move left')
-
-        elseif(currentPosition(2) < cordLimits(:,2))
-            %move down
+            counter = 0;
+            
+        elseif(currentPosition(4) < cordLimits(:,2))
+            %move up
             disp('move down')
+            counter = 0;
+            
         else
             %correct quad
             disp('Correct Quad')
+            counter = counter + 1;
+            if(counter >= 60)
+                correctQuad = 1;
+            end
+
         end
     otherwise
-
+        
 
 end
 
